1ceaede8a208c79c53d2229c305594904d0d0ad9
Emmanuel Eshun <91864345+eshunemma@users.noreply.github.com>
2021-12-03 05:49:58 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+class School{
+    constructor(name, level, numberOfStudents){
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get level() {
+        return this._level;
+    }
+
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+    
+    set numberOfStudents(studentsNumber) {
+        if (studentsNumber === Number) {
+            this.numberOfStudents = studentsNumber
+        } else {
+            console.log('Invalid input: numberOfStudents must be set to a Number.')
+        }
+    }
+
+    quickFacts() {
+        console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`)
+    }
+
+    static pickSubstituteTeacher(substituteTeachers){
+        const randomInteger = Math.floor(Math.random() * substituteTeachers.length);
+        return substituteTeachers[randomInteger]
+    }
+
+}
+
+class Primary extends School{
+    constructor(name, numberOfStudents, pickupPolicy){
+        super(name, 'primary', numberOfStudents);
+
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+}
+
+class HighSchool extends School{
+    constructor(name, numberOfStudents, sportsTeam){
+        super(name, 'high', numberOfStudents);
+        this._sportsTeam = sportsTeam;
+    }
+
+    get sportsTeam() {
+        return this._sportsTeam;
+    }
+}
+
+
+class MiddleSchool extends School{
+    constructor(name, numberOfStudents){
+        super(name, 'middle', numberOfStudents);
+    }
+}
+
+
+const lorraineHansbury = new Primary('Lorraine Hansbury', 54, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+
+const subTeacher = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+ const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+ console.log(alSmith.sportsTeam);
+
+ const yawEshun = new MiddleSchool('Yaw Eshun', 256);
+ yawEshun.quickFacts();
